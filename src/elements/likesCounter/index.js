c05bd903d1667e4c8e572245c143875545aa83a4
natalyamyunster <natalyamyunster@yahoo.com>
2021-03-26 14:19:19 +0100

    hw lesson2 task2

--- src/elements/likesCounter/index.js
+++ src/elements/likesCounter/index.js
@@ -1,7 +1,21 @@
 //Core
-import React from 'react';
+import React, {useState} from 'react';
 //Styles
 import Styles from './styles.module.scss';
 
-export const LikesCounter = ({counts}) =>  <span className={Styles.likesCounter}>{counts}</span>;
+export const LikesCounter = ({ counts }) => {
+    const [likes, setLikes] = useState(counts);
+
+    const changeLikes = () => {
+       likes > counts  ? setLikes(likes - 1) : setLikes(likes + 1) ;
+    };
+
+    return(
+        <span
+            className={Styles.likesCounter}
+            onClick={ changeLikes }
+        >{likes}</span>
+
+        )};
+
 
